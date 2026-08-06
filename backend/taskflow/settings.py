0324a978a1e02eb8fc869db4e007ad5d4d9cd927
rasool <hhhr208@gmail.com>
2026-08-07 01:18:01 +0330

    migrate: replace SQLite with PostgreSQL for production
    - Updated DATABASES settings to use PostgreSQL
    - Added psycopg2-binary to requirements
    - Removed SQLite config

--- backend/taskflow/settings.py
+++ backend/taskflow/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'taskflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'securepass123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
